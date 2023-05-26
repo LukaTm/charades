bf7b55c5b8a8588363ad6f663469b3d90727f4fc
LukaTm <119138345+LukaTm@users.noreply.github.com>
2023-05-26 23:43:53 +0300

    Sign up Validation

--- frontend/src/components/SignUpPage.js
+++ frontend/src/components/SignUpPage.js
@@ -2,31 +2,87 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./SignUpPage.css";
+import useInput from "../hooks/use-input";
 
 const SignUpPage = ({ rerun }) => {
-    const [nickname, setNickname] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
     const navigate = useNavigate();
 
-    const handleSignUp = async (e) => {
-        e.preventDefault();
-
-        if (password !== confirmPassword) {
-            alert("Passwords don't match");
-            return;
+    const isNotEmpty = (value) => value.trim() !== "";
+    const isEmail = (value) => value.includes("@");
+    const isPassword = (value) => {
+        // Minimum length requirement
+        if (value.length < 8) {
+            return false; // Password is too short
         }
 
+        if (value.length > 42) {
+            return false; // Password is too long
+        }
+
+        // use test for checking if value includes specific pattern
+        if (!/[A-Z]/.test(value)) {
+            return false; // Password does not contain an uppercase letter
+        }
+        // At least one number requirement
+        if (!/\d/.test(value)) {
+            return false; // Password does not contain a number
+        }
+
+        if (!/[!@#$%^&*]/.test(value)) {
+            return false; // Password does not contain a special character
+        }
+
+        return true;
+    };
+
+    const {
+        value: nicknameValue,
+        isValid: nicknameIsValid,
+        hasError: nicknameHasError,
+        valueChangeHandler: nicknameChangeHandler,
+        inputBlurHandler: nicknameBlurHandler,
+        reset: resetNickname,
+    } = useInput(isNotEmpty);
+    const {
+        value: passwordValue,
+        isValid: passwordIsValid,
+        hasError: passwordHasError,
+        valueChangeHandler: passwordChangeHandler,
+        inputBlurHandler: passwordBlurHandler,
+        reset: resetLastName,
+    } = useInput(isPassword);
+    const {
+        value: emailValue,
+        isValid: emailIsValid,
+        hasError: emailHasError,
+        valueChangeHandler: emailChangeHandler,
+        inputBlurHandler: emailBlurHandler,
+        reset: resetEmail,
+    } = useInput(isEmail);
+
+    let formIsValid = false;
+
+    if (nicknameIsValid && passwordIsValid && emailIsValid) {
+        formIsValid = true;
+    }
+
+    const handleSignUp = async (e) => {
         try {
+            e.preventDefault();
+            if (!formIsValid) {
+                return;
+            }
             const response = await axios.post(
                 "http://localhost:8080/api/signup",
                 {
-                    nickname,
-                    email,
-                    password,
+                    nicknameValue,
+                    emailValue,
+                    passwordValue,
                 }
             );
+            resetNickname();
+            resetLastName();
+            resetEmail();
 
             navigate("/login"); // Redirect
         } catch (error) {
@@ -35,6 +91,16 @@ const SignUpPage = ({ rerun }) => {
         }
     };
 
+    const nicknameClasses = nicknameHasError
+        ? "form-control invalid"
+        : "form-control";
+    const passwordClasses = passwordHasError
+        ? "form-control invalid"
+        : "form-control";
+    const emailClasses = emailHasError
+        ? "form-control invalid"
+        : "form-control";
+
     return (
         <div className="login-container">
             <h1 className="login-title">Sign Up</h1>
@@ -42,35 +108,58 @@ const SignUpPage = ({ rerun }) => {
                 <input
                     type="text"
                     placeholder="Nickname"
-                    value={nickname}
-                    onChange={(e) => setNickname(e.target.value)}
-                    required
-                    className="login-input"
+                    value={nicknameValue}
+                    onChange={nicknameChangeHandler}
+                    onBlur={nicknameBlurHandler}
+                    className={nicknameClasses}
                 />
+                {nicknameHasError && (
+                    <p className="error-text">Please enter a Nickname.</p>
+                )}
                 <input
                     type="email"
                     placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                    className="login-input"
+                    value={emailValue}
+                    onChange={emailChangeHandler}
+                    // WHEN LOSES FOCUS
+                    onBlur={emailBlurHandler}
+                    className={emailClasses}
                 />
+                {emailHasError && (
+                    <p className="error-text">Please enter a valid Email</p>
+                )}
                 <input
                     type="password"
                     placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                    className="login-input"
+                    value={passwordValue}
+                    onChange={passwordChangeHandler}
+                    onBlur={passwordBlurHandler}
+                    className={passwordClasses}
                 />
-                <input
+                {passwordHasError && (
+                    <p className="error-text">
+                        <div className="eight-characters-error">
+                            Be at least 8 characters<br></br>
+                        </div>
+                        <div className="one-capital-error">
+                            At least one capital letter<br></br>
+                        </div>
+                        <div className="one-number-error">
+                            At least one number<br></br>
+                        </div>
+                        <div className="one-special-character-error">
+                            At least one special character<br></br>
+                        </div>
+                    </p>
+                )}
+                {/* <input
                     type="password"
                     placeholder="Confirm Password"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                    required
-                    className="login-input"
-                />
+                    value={passwordClasses}
+                    onChange={passwordChangeHandler}
+                    onBlur={passwordBlurHandler}
+                    className={passwordClasses}
+                /> */}
                 <button type="submit" className="login-button">
                     Sign Up
                 </button>
